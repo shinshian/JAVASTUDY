67197a65a8517b7667cb682f42e7257a6a0b9717
shinshian <shinshian2009@gmail.com>
2025-12-01 18:16:57 +0900

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[]args){
+        System.out.println("B등급");
+        for (int i= 1; i<=3; i++){
+            System.out.println(i + "번째 print!!!");
+
+        }
+    }
+}
